b611209f07ef1ea91b8463975ccffb2b411fa07e
21toffy <oketofoke@gmail.com>
2020-09-11 20:54:03 +0100

    views.py

--- courses/views.py
+++ courses/views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 
 
 def course_list(request):
+    courses = Course.objects.all()
+    context = {'courses':courses}
+    template = 'course/course_list.html'
+    return render(request, template, context)
+    
+
+def course_detail (request, pk):
+    
     pass
+
+
 
 def course_register(request):
     pass
@@ -11,6 +21,4 @@ def course_register(request):
 
 
 
-def course_detail (request):
-    pass
 
